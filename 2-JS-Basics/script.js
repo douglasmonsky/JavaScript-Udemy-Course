2b6bd24b9e1d3da6fc61876fef7970174a182ac2
Douglas Monsky <douglas.monsky@gmail.com>
2018-03-16 15:38:42 -0400

    Section 2, Lecture 10
    More on variables.
    NEW - prompt and alert.

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,3 +1,5 @@
+// Lecture: variables
+/*
 var name = 'Doug';
 console.log(name);
 
@@ -9,3 +11,33 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge)
+*/
+
+// Lecture: variables 2
+var name = 'Doug';
+var age = 24;
+console.log(name + age);
+console.log(age + age);
+
+var job, isMarried;
+console.log(job);
+
+job = 'programmer';
+isMarried = false;
+// console.log(job);
+// console.log(isMarried);
+
+console.log(name + ' is a ' + age + ' year old '
+     + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty-six';
+job = 'psychologist';
+
+console.log(name + ' is a ' + age + ' year old '
+     + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old '
+     + job + '. Is he married? ' + isMarried + '.');
