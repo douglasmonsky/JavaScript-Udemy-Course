b103025f799c544557cb027b5f0120223b462257
Douglas Monsky <douglas.monsky@gmail.com>
2018-03-16 15:50:53 -0400

    Section 2, Lecture 11
    Operators

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -14,30 +14,38 @@ console.log(fullAge)
 */
 
 // Lecture: variables 2
-var name = 'Doug';
-var age = 24;
-console.log(name + age);
-console.log(age + age);
+// var name = 'Doug';
+// var age = 24;
+// console.log(name + age);
+// console.log(age + age);
 
-var job, isMarried;
-console.log(job);
-
-job = 'programmer';
-isMarried = false;
+// var job, isMarried;
 // console.log(job);
-// console.log(isMarried);
 
-console.log(name + ' is a ' + age + ' year old '
-     + job + '. Is he married? ' + isMarried + '.');
+// job = 'programmer';
+// isMarried = false;
+// // console.log(job);
+// // console.log(isMarried);
 
-age = 'thirty-six';
-job = 'psychologist';
+// console.log(name + ' is a ' + age + ' year old '
+//      + job + '. Is he married? ' + isMarried + '.');
 
-console.log(name + ' is a ' + age + ' year old '
-     + job + '. Is he married? ' + isMarried + '.');
+// age = 'thirty-six';
+// job = 'psychologist';
 
-var lastName = prompt('What is the last name?');
-console.log(lastName);
+// console.log(name + ' is a ' + age + ' year old '
+//      + job + '. Is he married? ' + isMarried + '.');
 
-alert(name + ' is a ' + age + ' year old '
-     + job + '. Is he married? ' + isMarried + '.');
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
+
+// alert(name + ' is a ' + age + ' year old '
+//      + job + '. Is he married? ' + isMarried + '.');
+
+
+//Lecture: operators
+var Now = 2018;
+var age = 23;
+var birthYear = Now - age;
+
+console.log(birthYear);
