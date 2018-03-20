3f308af775a9ac320953ae1af73fd524b08b5ebf
Douglas Monsky <douglas.monsky@gmail.com>
2018-03-20 17:18:24 -0400

    Section 5, Lecture 58: IIFE

--- 5-Advanced-JS/firstClassFunctions.js
+++ 5-Advanced-JS/firstClassFunctions.js
@@ -54,3 +54,24 @@ function interviewQuestion(job) {
 
 var teacherQuestion = interviewQuestion('teacher');
 teacherQuestion('Doug');
+
+//Lecture: Immediately Invoked Function Expressions(IIFE)
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+game();
+
+(function () {    //this is actual IIFE, previous is the decleration equivalent.
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+
+(function (goodluck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodluck);
+})(5);
+
+(showName = function (name) {console.log(name || "No Name")}) (); // No Name​
+showName ("Rich"); // Rich​
+showName (); // No Name
