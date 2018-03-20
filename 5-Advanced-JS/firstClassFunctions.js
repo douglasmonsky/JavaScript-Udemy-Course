b8e7909d1c05e35da0d29cf53cbd81c94bfeadf8
Douglas Monsky <douglas.monsky@gmail.com>
2018-03-20 16:55:50 -0400

    Section 5, Lecture 58: More on first class functions

--- 5-Advanced-JS/firstClassFunctions.js
+++ 5-Advanced-JS/firstClassFunctions.js
@@ -33,3 +33,24 @@ var maxHeartRates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(maxHeartRates);
+
+//Lecture: Functions returning functions
+
+function interviewQuestion(job) {
+    if (job ==='designer') {
+        return function(name) {
+            console.log(name + ' can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log(name + ', what subject do you teach?');
+        }
+    } else {
+        return function(name) {
+            console.log( ' Hello ' + name + ' what do you do?')
+        }
+     }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+teacherQuestion('Doug');
