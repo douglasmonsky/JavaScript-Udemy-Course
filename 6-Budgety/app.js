1291e7f7519fb3ab8a2907e8ca94e2e34c691d89
Douglas Monsky <douglas.monsky@gmail.com>
2018-03-21 20:44:02 -0400

    Section 6, Lecture 74: Add Items to UI

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -68,7 +68,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -79,6 +81,31 @@ var UIController = (function() {
                 value: document.querySelector(DOMstrings.inputValue).value,
             };
         },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+            if (type === 'inc'){
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id$"> <div class="item__description">%description%' +
+                '</div> <div class="right clearfix"> <div class="item__value">%value%</div>' +
+                '<div class="item__delete"> <button class="item__delete--btn">' + 
+                '<i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div>' +
+                '<div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div>' +
+                '<div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>' +
+                 '</div></div></div>';
+            }
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+        },
+
         getDOMstring: function() {
             return DOMstrings;
         }
@@ -101,9 +128,7 @@ var UIController = (function() {
 var controller = (function(budgetCtrl, UIctrl) {
     var setupEventListeners = function() {
         var DOMstrings = UIctrl.getDOMstring();
-
         document.querySelector(DOMstrings.inputBtn).addEventListener('click', ctrlAddItem);
-
         document.addEventListener('keypress', function(event) {
             if (event.key === 'Enter') {
                 ctrlAddItem();
@@ -117,6 +142,7 @@ var controller = (function(budgetCtrl, UIctrl) {
 
         input = UIctrl.getinput();
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        UIctrl.addListItem(newItem, input.type);
         console.log(input);
     }
 
@@ -129,4 +155,6 @@ var controller = (function(budgetCtrl, UIctrl) {
 })(budgetController, UIController);
 
 
-controller.init();
+(function() { 
+    controller.init();
+})();
